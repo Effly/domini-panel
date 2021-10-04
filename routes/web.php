83091472c996e68a28a64f71af9f6bab37778496
Effly <effly88@gmail.com>
2021-10-04 09:15:21 +0300

    Добавить ajax сортировку Добавить валидацию, что при slider=big, обязательное поле image_for_ipad Добавить update Добавить destroy Добавить метод рендера страницы создания Добавить метод для ajax валидации изображения Добавить метод ajax валидации(request 200) ссылок

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\SlidersController;
+use App\Http\Controllers\SeparatorController;
+use App\Http\Controllers\MainPageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +16,21 @@ use App\Http\Controllers\AdminController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/admin',[AdminController::class,'index']);
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', [MainPageController::class,'index'])->name('welcome');
+Route::get('/admin',[AdminController::class,'index'])->name('root');
+Route::get('/create-page',[AdminController::class,'show_create_page'])->name('show_create_page');
+Route::post('/create',[AdminController::class,'store'])->name('store_new');
+Route::get('/show/{id}',[AdminController::class,'show'])->name('show');
+Route::post('/update',[AdminController::class,'update'])->name('update');
+Route::get('/delete/{id}',[AdminController::class,'destroy'])->name('delete');
+Route::post('/image-check',[AdminController::class,'imageCheck'])->name('image-check');
+Route::post('/link-check',[AdminController::class,'linkCheck'])->name('link-check');
+Route::get('/sliders',[SlidersController::class,'index'])->name('sliders');
+Route::post('/update-slide',[SlidersController::class,'update'])->name('update-slide');
+Route::post('/update-speed',[SlidersController::class,'updateSpeed'])->name('update-speed');
+Route::post('/update-label',[SlidersController::class,'updateLabel'])->name('update-label');
+Route::get('/separator',[SeparatorController::class,'index'])->name('separator');
+Route::post('/separator-update',[SeparatorController::class,'update'])->name('separator-update');
