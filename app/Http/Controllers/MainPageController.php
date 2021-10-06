f8cc5a61d79ba427e3414ac3610bbdebd5cc153c
Effly <effly88@gmail.com>
2021-10-06 18:01:10 +0300

    Добавить скорость для слайдеров

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Games;
 use App\Models\Separator;
+use App\Models\Settings;
 use App\Models\Slides;
 use Illuminate\Http\Request;
 use Jenssegers\Agent\Agent;
 
 class MainPageController extends Controller
 {
-    public function index(Games $games, Slides $slides, Separator $separator)
+    public function index(Games $games, Slides $slides, Separator $separator,Settings $settings)
     {
         $agent = new Agent();
         $version = $agent->isTablet();;
@@ -29,6 +30,15 @@ class MainPageController extends Controller
         $data_for_small = array_slice($result, 3);
 //        dd($data_for_big);
         $data_for_separator = $separator->find(1);
-        return view('welcome', ['data_for_big' => $data_for_big, 'data_for_small' => $data_for_small, 'data_for_separator' => $data_for_separator,'version'=>$version]);
+        $speed_for_big = $settings->where('name','big')->first()->speed;
+        $speed_for_small = $settings->where('name','small')->first()->speed;
+        return view('welcome', [
+            'data_for_big' => $data_for_big,
+            'data_for_small' => $data_for_small,
+            'data_for_separator' => $data_for_separator,
+            'version'=>$version,
+            'speed_for_big'=>$speed_for_big,
+            'speed_for_small'=>$speed_for_small
+            ]);
     }
 }
