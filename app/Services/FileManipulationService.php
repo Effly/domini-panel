98938442c0fe9a0891066e339a90b7291f7edb61
Effly <effly88@gmail.com>
2021-10-28 09:38:00 +0300

    Добавлена проверка на наличие файла, универсальный метод загрузки файлов, геттер пути до папки.

--- app/Services/FileManipulationService.php
+++ app/Services/FileManipulationService.php
@@ -25,23 +25,21 @@ class FileManipulationService
 
         $dir_name = str_replace($gameData->tech_name.'.png','',$gameData->image_name);
         Storage::disk('public')->deleteDirectory($dir_name);
-//        if (Storage::disk('public')->exists($gameData->image_name_ipad)) {
-//            Storage::disk('public')->delete($gameData->image_name_ipad);
-//        }
-
         return $gameData->name;
 
     }
 
-    public function uploadImage($data){
-        if (isset($data['image'])){
-            return Storage::disk('public')->putFileAs('Images/' . $data['platform'] . '/' . $data['tech_name'], $data['image'], $data['tech_name'] . '.png');
-        }
+    public function uploadImage($path,$file,$filename){
+            return Storage::disk('public')->putFileAs($path,$file,$filename);
     }
 
-    public function uploadImageIpad($data){
-        if (isset($data['image_for_ipad'])) {
-            return Storage::disk('public')->putFileAs('Images/' . $data['platform'] . '/' . $data['tech_name'], $data['image_for_ipad'], $data['tech_name'] . '_ipad.png');
-        }
+
+    public function hasFileDir($id){
+       return Storage::disk('public')->has($this->game->getDataGameById($id)->image_name);
     }
+
+    public function getPublicDirPath($name){
+        return Storage::disk()->url($name);
+    }
+
 }
